7b59ac044e6069aae382ffe0d1c3dffaf77759d9
Berbe <berbedeblauwe@gmail.com>
2022-06-24 18:17:24 +0200

    Project page with content

--- frontend/audit/src/app/pages/project/project.component.ts
+++ frontend/audit/src/app/pages/project/project.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
+import { Project } from 'src/app/models/project';
+import { ProjectInfo } from 'src/app/models/project-info';
 
 @Component({
   selector: 'app-project',
@@ -16,6 +18,19 @@ export class ProjectComponent implements OnInit {
 
   ngOnInit(): void {
     this.projectId = this.route.snapshot.paramMap.get('projectid');
+    this.project = {'id': this.projectId, 'projectInfo': this.projectInfo} as Project
   }
 
+  private projectInfo: ProjectInfo = {
+    'userId': '1',
+    'productiveUnit': 'Project 1',
+    'country': 'Belgium',
+    'region': 'West-Vlaanderen',
+    'municipality': 'municipality',
+    'producer': 'producer',
+    'contactDetails': 'Telefoon',
+    'confidentialInformation': 'confidential',
+  };
+
+  project: Project
 }
