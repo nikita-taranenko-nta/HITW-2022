ab026e9035da4128f59a55ba5c746cdb60b8b3b8
Nikita Taranenko <nta@nexiosit.com>
2022-06-24 13:32:43 +0200

    Add pseudocode to programs.cs

--- backend/HITW_api/Program.cs
+++ backend/HITW_api/Program.cs
@@ -5,3 +5,21 @@ app.MapGet("/", () => "Hello World!");
 app.MapGet("/other", () => "Other Hello World!");
 
 app.Run();
+
+
+POST /projects
+PUT  /projects/{id}
+
+- UserId: int
+        - Productive unit: string Companyname
+        - Country: 2 letters
+        - Region/Province: string
+        - Municipality/District: string
+        - Producer: Boer -> search after 3 characters
+        - Contact details: free text
+        - Confidential information: Free text (big text area)
+	
+GET /projects
+    /projects/{id}
+
+
