b67ac492faf4595045a5c0158ef9e9a50ee0b2d1
BirgenV <birgenvermang@gmail.com>
2022-06-25 12:37:56 +0200

    fixed errors after merge

--- SqlScripts/AddData.sql
+++ SqlScripts/AddData.sql
@@ -15,27 +15,27 @@ VALUES ('my little theme name');
 INSERT INTO Theme (Name)
 VALUES ('my little theme name 2: the themening');
 
-INSERT INTO Question (question_name,question_string,theme_id)
+INSERT INTO Question (Name,Data,ThemeId)
 VALUES ('flying speed bird coconuts please?', 'flying speed bird coconuts please?', 5);
-INSERT INTO Question (question_name,question_string,theme_id)
+INSERT INTO Question (Name,Data,ThemeId)
 VALUES ('Do you feel lucky?', 'Do you feel lucky?', 6);
 
-INSERT INTO Theme_score (theme_id,project_id,score)
+INSERT INTO ThemeScore (ThemeId,ProjectId,Score)
 VALUES (5, 1, 2);
-INSERT INTO Theme_score (theme_id,project_id,score)
+INSERT INTO ThemeScore (ThemeId,ProjectId,Score)
 VALUES (5, 1, 3);
-INSERT INTO Theme_score (theme_id,project_id,score)
+INSERT INTO ThemeScore (ThemeId,ProjectId,Score)
 VALUES (5, 2, 4);
-INSERT INTO Theme_score (theme_id,project_id,score)
+INSERT INTO ThemeScore (ThemeId,ProjectId,Score)
 VALUES (6, 2, 0);
 
-INSERT INTO Answer (answer_string,theme_score_id,question_id)
+INSERT INTO Answer (Data,ThemeScoreId,QuestionId)
 VALUES ('Nikiti has a dirty hat', 1, 27);
-INSERT INTO Answer (answer_string,theme_score_id,question_id)
+INSERT INTO Answer (Data,ThemeScoreId,QuestionId)
 VALUES ('thx thomas for the beerz', 1, 28);
-INSERT INTO Answer (answer_string,theme_score_id,question_id)
+INSERT INTO Answer (Data,ThemeScoreId,QuestionId)
 VALUES ('we all love c# and strongly dislike java', 2, 27);
-INSERT INTO Answer (answer_string,theme_score_id,question_id)
+INSERT INTO Answer (Data,ThemeScoreId,QuestionId)
 VALUES ('i have no more inspiration', 2, 28);
 
 --todo add submission and commitment
