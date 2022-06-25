0fd5e55e1b22c5ea76db4c9979110129e05ad04a
Berbe <berbedeblauwe@gmail.com>
2022-06-25 12:13:57 +0200

    Showing questions on the project-scores page

--- frontend/audit/src/app/components/score-question/score-question.component.ts
+++ frontend/audit/src/app/components/score-question/score-question.component.ts
@@ -2,13 +2,13 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Question } from 'src/app/models/question';
 
 @Component({
-  selector: 'app-score-question',
+  selector: 'score-question',
   templateUrl: './score-question.component.html',
   styleUrls: ['./score-question.component.scss']
 })
 export class ScoreQuestionComponent implements OnInit {
   @Input() public question: Question
-  constructor() { }
+  constructor() { console.log(this.question) }
 
   ngOnInit(): void {
   }
