00766a9b189c0012b2815e189fad122de4099342
Laurens Van De Walle <lvdw@nexiosit.com>
2022-06-25 10:25:52 +0200

    Get project-scores-component project field value from backend

--- frontend/audit/src/app/pages/project-scores/project-scores.component.ts
+++ frontend/audit/src/app/pages/project-scores/project-scores.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Project } from 'src/app/models/project';
-import { ProjectInfo } from 'src/app/models/project-info';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {Project} from 'src/app/models/project';
+import {ProjectService} from "../../shared/services/project.service";
 
 @Component({
   selector: 'app-project-scores',
@@ -11,22 +11,16 @@ import { ProjectInfo } from 'src/app/models/project-info';
 export class ProjectScoresComponent implements OnInit {
   public projectId: string;
   public project: Project;
-  
-  constructor(private route: ActivatedRoute) { }
+
+  constructor(
+    private readonly projectService: ProjectService,
+    private readonly route: ActivatedRoute
+  ) {
+  }
 
   ngOnInit(): void {
     this.projectId = this.route.snapshot.paramMap.get('projectid');
-    this.project = {'id': this.projectId, 'projectInfo': this.projectInfo} as Project
+    this.projectService.getProject(this.projectId)
+      .subscribe(project => this.project = project);
   }
-
-  private projectInfo: ProjectInfo = {
-    'userId': '1',
-    'productiveUnit': 'Project 1',
-    'country': 'Belgium',
-    'region': 'West-Vlaanderen',
-    'municipality': 'municipality',
-    'producer': 'producer',
-    'contactDetails': 'Telefoon',
-    'confidentialInformation': 'confidential',
-  };
 }
