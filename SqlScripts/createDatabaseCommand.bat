17f0269e27abf5d12f7da9311bd10b331ec05ffb
Lustwelpintje <stephanevyt@hotmail.com>
2022-06-25 16:55:20 +0200

    delete useless data

--- SqlScripts/createDatabaseCommand.bat
+++ SqlScripts/createDatabaseCommand.bat
@@ -1,4 +1,3 @@
 docker exec -i mssqlcontainer /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P NexiosIT1234 < CreateDatabase.sql
 docker exec -i mssqlcontainer /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P NexiosIT1234 < AddQuestions.sql
-docker exec -i mssqlcontainer /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P NexiosIT1234 < AddData.sql
 pause
