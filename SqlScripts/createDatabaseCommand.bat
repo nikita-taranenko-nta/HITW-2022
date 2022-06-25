e692b82513142394f2fcb02b2bb9a9f2e6d88b52
Nikita Taranenko <nta@nexiosit.com>
2022-06-25 10:07:45 +0200

    Add containername to Docker script

--- SqlScripts/createDatabaseCommand.bat
+++ SqlScripts/createDatabaseCommand.bat
@@ -1,2 +1,3 @@
-docker exec -i elated_dirac /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P NexiosIT1234 < CreateDatabase.sql
-docker exec -i elated_dirac /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P NexiosIT1234 < AddData.sql
+docker exec -i mssqlcontainer /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P NexiosIT1234 < CreateDatabase.sql
+docker exec -i mssqlcontainer /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P NexiosIT1234 < AddData.sql
+pause
